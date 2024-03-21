806c9353b3f6d3f2f23e10fd52385203261110c4
Dom McLaughlin <dom@housemates.io>
2024-03-21 15:07:29 +0000

    feat: add group exclusive data

--- generate.sh
+++ generate.sh
@@ -3,7 +3,7 @@
 export PHP_POST_PROCESS_FILE="/usr/local/bin/prettier --write"
 
 # Run openapi-generator-cli
-openapi-generator-cli generate -i ~/Code/housemates-connect-api/housemates-connect.yaml -g php -o ./openapi_generated_connect --global-property skipFormModel=false
+openapi-generator-cli generate -i /Users/dom/Documents/dev-herd/housemates-connect/housemates-connect.yaml -g php -o ./openapi_generated_connect --global-property skipFormModel=false
 
 # Copy files from lib folder to codegen folder
 cp -r ./openapi_generated_connect/lib/* ./codegen/
